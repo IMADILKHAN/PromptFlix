b0d6a1c2266d3b99c39d3ab6f58caaf90aef9629
IMADILKHAN <blueaadil@gmail.com>
2025-07-15 18:19:14 +0530

    Added Auth Utils

--- promptflix/src/components/LoginPage.jsx
+++ promptflix/src/components/LoginPage.jsx
@@ -1,12 +1,23 @@
 import { Header } from "./Header";
 import bgImg from "../assets/backgroundImage.png"
-import { useState } from "react";
+import { useState,useRef } from "react";
+import { checkValidData } from "../utils/validate";
 
 export function LoginPage(){
-    const [status,setStatus] = useState(true);
+    const [status,setStatus] = useState(true); 
+    const [errorMsg,serErrorMsg] = useState(null);
+    const email = useRef(null);
+    const password = useRef(null);
+
     function toggleSignin(){
         setStatus(!status)
         console.log(status)
+    } 
+    function HandleButtonClick(){
+        // Validate Form Data
+        const msg = checkValidData(email.current.value,password.current.value)
+        serErrorMsg(msg)
+        console.log(msg);
     }
     return <>
         <div className="absolute inset-0 w-full h-full object-cover z-0" >
@@ -15,15 +26,16 @@ export function LoginPage(){
             </div>
             <img src={bgImg} alt="" />
         </div>
-        <form action="" className=" w-3/12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 p-12 rounded-lg shadow-lg z-10 h-100 flex flex-col gap-4">
+        <form onSubmit={(e)=>e.preventDefault()}  action="" className=" w-3/12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-70 p-12 rounded-lg shadow-lg z-10 h-120 flex flex-col gap-4">
             <h2 className="text-white pt-0 mt-0" >{status? "Sign In" : "Sign Up"}</h2>
             { !status && (
                 <input type="text"  placeholder="Full Name" className="p-2 my-2 block w-full rounded bg-white text-black" />                
                 )
             }
-            <input type="text"  placeholder="Email Adress" className="p-2 my-2 block w-full rounded bg-white text-black" />
-            <input type="password" placeholder="Password" className="p-2 my-2 block w-full rounded bg-white text-black" />
-            <button className="p-2 mt-4 w-full bg-red-600 rounded-md hover:bg-red-700 transition duration-200" >{status? "Sign In": "Sign Up"}</button>
+            <input ref={email}  type="text"  placeholder="Email Adress" className="p-2 my-2 block w-full rounded bg-grey-600 text-black" />
+            <input ref={password} type="password" placeholder="Password" className="p-2 my-2 block w-full rounded bg-grey-600 text-black" />
+            <p className="text-red-500">{errorMsg}</p>
+            <button onClick={HandleButtonClick} className="p-2 mt-4 w-full bg-red-600 rounded-md hover:bg-red-700 transition duration-200" >{status? "Sign In": "Sign Up"}</button>
             <p className="p-2 text-white cursor-pointer" onClick={toggleSignin} >{status? "New to PromptFlix? Sign Up!" : "Already a User? Sign In!"}</p>
         </form>
     </>
---
+++ promptflix/src/utils/validate.js
@@ -0,0 +1,12 @@
+export function checkValidData(email,password){
+    let isEmailValid  = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(email);
+    let isPasswordValid = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(password);
+
+    if(!isEmailValid){
+        return "Email Invalid"
+    }
+    if(!isPasswordValid){
+        return "Password Invalid"
+    }
+
+}
